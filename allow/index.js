cca393bcdf2eb19348a992b450faa9dfaf110d28
Ludovic Urbain <ludovic.urbain.1@gmail.com>
2022-12-10 14:27:29 +0100

    first cleanup

--- allow/index.js
+++ allow/index.js
@@ -18,7 +18,8 @@ function on_init_lists(){
 }
 
 function preload_data(){
-	var a=['season','week','sport','teacher','people'];
+	var a=[];
+	//var a=['season','week','sport','teacher','people'];
 	for(var i=0;i<a.length;i++){
 		preload_for_iclass(a[i]);
 	}
@@ -33,7 +34,7 @@ $(document).on('click','.bg',function(){
 });
 
 $(document).on('click','#menu div',function(){
-	route('#'+$(this).html());
+	route('#'+$(this).attr('route'));
 });
 
 function create_modal(modal_content,attributes={}){
@@ -58,33 +59,10 @@ function delete_modal(){
 
 $(document).on('click','.modal_glasspane,.close_modal',delete_modal);
 
-callbacks['item_card_delete']['stage']=callbacks['item_card_edit']['stage'] = function(){
-	preload_data();
-	route();
-}
-callbacks['item_card_delete']['week']=callbacks['item_card_edit']['week'] = function(){
-	preload_data();
-	route();
-}
-callbacks['item_card_delete']['season']=callbacks['item_card_edit']['season'] = function(){
-	preload_data();
-	route();
-}
-callbacks['item_card_delete']['teacher']=callbacks['item_card_edit']['teacher'] = function(){
+register_callback(['item_card_edit','category'],function(o){
 	delete_modal();
-}
-callbacks['item_card_delete']['people']=callbacks['item_card_edit']['people'] = function(){
-	delete_modal();
-}
-callbacks['item_card_delete']['payment']=function(){	
-	delete_modal();
-	$('.table_list_line[iid="'+people_iid+'"][iclass="people"]').click();
-}
-callbacks['item_card_edit']['payment'] = function(data){
-	api.add_relation({'iclass':'people','iid':people_iid,'iclass_2':'payment','iid_2':data});
-	delete_modal();
-	setTimeout(function(){$('.table_list_line[iid="'+people_iid+'"][iclass="people"]').click();},500);
-}
+	route('',false,true);
+});
 
 function generic_new_item_modal(c,app_string,relations={}){
 	api.empty_item({ 'iclass': c['iclass_1'] }, function (data) {
@@ -93,24 +71,15 @@ function generic_new_item_modal(c,app_string,relations={}){
 }
 
 painters['menu']=function(){
-	var menu=Object.keys(states);
 	var s='';
-	for(var i=0;i<menu.length;i++){
-		s+='<div>'+menu[i]+'</div>';
+	for(var route in states){
+		s+='<div route="'+route+'">'+states[route]['label']+'</div>';
 	}
 	return '<div id=menu>'+s+'</div>';
 };
 
 bubble_click('.button.create_related[iclass_1="season"]',function(c,t){
 	generic_new_item_modal(c,'Nouvelle période');
-});
-
-bubble_click('.button.create_related[iclass_1="teacher"]',function(c,t){
-	generic_new_item_modal(c,'Nouveau moniteur',{"sport":{"iid": 1, "relation_key": "description", "relation_label": 'Sport' }});
-});
-
-bubble_click('.button.create_related[iclass_1="people"]',function(c,t){
-	generic_new_item_modal(c,'Nouveau participant');
 });
 
 function specific_actions(iclass, item){
@@ -131,276 +100,6 @@ function specific_actions(iclass, item){
 	}
 	return html;
 }
-
-$(document).on('click','.duplicate',function(){
-	$(this).parents('.item_card').attr('iid','');
-	$(this).parents('.item_card').find('.item_cardSave').click();
-	return false;
-});
-
-var people_iid=null;
-$(document).on('click','.select_new_stage',function(){
-	$(this).parents('.table_list_line').addClass('selected');
-	create_modal(
-		create_ui_element({
-			'ui_element':{
-				'element_class':'table_list',
-				'selected_iids':[],
-				'hidden':['level','price'],
-				'actions':[{'CSSclass':'sign_up_for_stage','name':'Inscrire à ce stage'}]
-			},
-			'api_call':{'pa':{'m':'search_item','iclass':'stage','search':''},'postcontent':{}}
-		})
-	);
-	return false;
-});
-
-$(document).on('click','.add_payment',function(){
-	var c={'iclass_1':'payment','iclass_2':'stage','iid_2':$(this).parents('[iid]').attr('iid')};
-	generic_new_item_modal(c,'Enregistrer un payement');
-	return false;
-});
-
-
-
-$(document).on('click','.sign_up_for_stage',function(){
-	api['add_relation']({'iclass':'people','iid':people_iid,'iclass_2':'stage','iid_2':$(this).parents('.table_list_line').attr('iid')},{},function(){
-		delete_modal();
-		$('.table_list_line[iid="'+people_iid+'"][iclass="people"]').click();
-	});
-	return false;
-});
-
-$(document).on('click','.table_list_line[iclass="stage"]',function(){
-	switch($('#menu .selected').text()){
-		case 'stages':
-			api.get_item_with_relations(
-				{'iclass':'stage','iid':$(this).attr('iid')},
-				function (data) {
-					create_modal(
-						readitem_card('stage', {'item': data[0]['item'],'relations':{"sport":{"iid": data[0]['relations']['sport']['iid'], "relation_key": "description", "relation_label": 'Sport' },"teacher":{"iid": data[0]['relations']['teacher']['iid'], "relation_key": "description", "relation_label": 'Moniteur' }}})
-					);
-				}
-			);
-			break;
-		case 'paiements':
-			people_iid=$(this).parents('[container]').first().prev().attr('iid');
-			if($(this).children('[name="description"][value="Assurance"]').length>0){
-				open_people_modal();
-			}else{
-				var c={'iclass_1':'payment','iclass_2':'stage','iid_2':$(this).attr('iid')};
-				generic_new_item_modal(c,'Enregistrer un payement');
-			}
-			break;
-		default:
-			break;
-	}
-	return false;
-});
-
-$(document).on('click','.table_list_line[iclass="people"]',function(){
-	people_iid=$(this).attr('iid');
-	open_people_modal();
-	return false;
-});
-
-function open_people_modal(){
-	api.get_item(
-		{'iclass':'people','iid':people_iid},
-		function (data) {
-			create_modal(
-				'<div class=title>Editer un participant</div>'+
-				readitem_card('people', {'item': data[0]})+
-				create_ui_element(
-					{
-						'ui_element':{
-							'CSSclass':'people_stages',
-							'element_class':'table_list',
-							'selected_iids':[],
-							'actions':[{'CSSclass':'add_payment','name':'Ajouter un payement'}],
-							'hidden':['css_class'],
-							'inline_attributes':['css_class']
-						},
-						'api_call':{
-							'pa':{
-								'id':'','description': 'get_people_stages','db_id': 'this','m':'run_query'
-							},
-							'postcontent': {
-								'query_parameters': {'people_id': people_iid }
-							}
-						}
-					}
-				)
-			);
-		}
-	);
-}
-
-$(document).on('click','[element_class="table_list"] [iclass="teacher"][iid]',function(){
-	var iid=$(this).attr('iid');
-	api.get_item_with_relations(
-		{'iclass':'teacher','iid':iid},
-		function (data) {
-			create_modal(
-				'<div class=title>Editer un moniteur</div>'+
-				readitem_card('teacher', {'item': data[0]['item'],'relations':{"sport":{"iid": data[0]['relations']['sport']['iid'], "relation_key": "description", "relation_label": 'Sport' }}})+
-				create_ui_element({
-				'ui_element':{
-					'CSSclass':'teacher_availability',
-					'element_class':'table_list',
-					'selected_iids':[],
-					'hidden':['css_class'],
-					'inline_attributes':['css_class']
-				},
-				'api_call':{'pa':{'id':'','description': 'get_teacher_availability','db_id': 'this','m':'run_query'},'postcontent': {'query_parameters': {'teacher_id': iid }}}}));
-		}
-	);
-	return false;
-});
-
-$(document).on('click','.teacher_availability .table_list_line',function(){
-	var teacher_line=$('.teacher_list .table_list_line.selected');
-	var method='add_relation';
-	if($(this).attr('css_class')=='green'){
-		method='delete_relation';
-	}
-	api[method](
-		{'iclass':'teacher','iid':teacher_line.attr('iid'),'iclass_2':'week','iid_2':$(this).attr('iid')},{},function(){
-		refresh_ui_element('table_list',$('.teacher_availability').attr('uuid'));
-	});
-});
-
-$(document).on('click','.button.edit_season,.button.edit_week,.button.edit_stage',function(){
-	var iclass=$(this).attr('iclass');
-	api.get_item(
-		{'iclass':iclass,'iid':$(this).attr('iid')},
-		function (data) {
-			create_modal(readitem_card(iclass, {'item': data[0]}, 'div'));
-		}
-	);
-	return false;
-});
-$(document).on('click','.button.add_week',function(){
-	var c={'iclass_1':'week','iclass_2':'season','iid_2':$(this).attr('iid')};
-	generic_new_item_modal(c,'Nouvelle semaine');
-	return false;
-});
-
-$(document).on('click','.button.add_stage',function(){
-	var c={'iclass_1':'stage','iclass_2':'week','iid_2':$(this).attr('iid')};
-	generic_new_item_modal(c,'Nouveau stage',{
-		"sport":{"relation_key": "description", "relation_label": 'Sport' }
-	});
-	return false;
-});
-
-$(document).on('click','.button.add_participant',function(){
-	//autocomplete people
-	var people_autocomplete='\
-	<div class="autocomplete">\
-		<input autocomplete="off" placeholder="chercher" type="text" target="people" search_key="firstname|lastname|phone" name="add_participant">\
-	  <input type="hidden" class="add_and_relate_participant" name="id" stage_id="'+$(this).attr('iid')+'">\
-	</div>';
-	//new modal
-	create_modal(people_autocomplete,{'modal_type':'add_participant'});
-	setTimeout(function(){$('[autocomplete]').focus();},100);
-});
-
-$(document).on('change','.add_and_relate_participant',function(){
-	//select one > relate to stage > o happy day.
-	show_loader();
-	api.add_relations(
-		{
-			'iclass':'people','iid':$(this).val()
-		},
-		{
-			'relations':
-				{
-					'stage':[$(this).attr('stage_id')]
-				}
-		},
-		function(){
-			hide_loader();
-			route();
-		}
-	);
-});
-
-$(document).on('click','.remove_participant',function(){
-	//select one > relate to stage > o happy day.
-	show_loader();
-	api.delete_relations(
-		{
-			'iclass':'people','iid':$(this).parents('[iclass="people"]').attr('iid')
-		},
-		{
-			'relremove':
-				{
-					'stage':[$(this).parents('[container]').first().prev().attr('iid')]
-				}
-		},
-		function(){
-			hide_loader();
-			route();
-		}
-	);
-	return false;
-});
-
-
-
-$(document).on('click','.button.add_abo',function(){
-	var c={'iclass_1':'stage','iclass_2':'season','iid_2':$(this).attr('iid')};
-	generic_new_item_modal(c,'Nouvel abonnement',{"sport":{"iid": 1, "relation_key": "description", "relation_label": 'Sport' }});
-	return false;
-});
-
-$(document).on('click','.button.add_rental',function(){
-	var c={'iclass_1':'stage','iclass_2':'season','iid_2':$(this).attr('iid')};
-	generic_new_item_modal(c,'Nouvelle location',{"sport":{"iid": 1, "relation_key": "description", "relation_label": 'Sport' }});
-	return false;
-});
-
-$(document).on('click','.print_stage',function(){
-	var iids=[];
-	switch($(this).parents('[iclass]').attr('iclass')){
-		case 'stage':
-			iids.push($(this).parents('.table_list_line').attr('iid'));
-			break;
-		case 'sport':
-			var target=$(this).parents('[description]').first().attr('description');
-			if($(this).parents('[iclass="sport"]').attr('description')!='piscine'){
-				$(this).parents('[container]').first().find('[container="'+target+'"] .table_list_line[iid]').each(function(){
-					iids.push($(this).attr('iid'));
-				});
-			}
-			apiCall(
-				{'m':'generate_week_summary'},
-					function(data){
-						download_file(data);
-					}
-				,{'order':'p.lastname,p.firstname,t2.level','sport_id':$(this).parents('[iclass]').attr('iid'), 'week_id':$('[iclass="week"][description="'+$(this).parents('[container]').first().attr('container')+'"]').attr('iid')});
-				
-			apiCall(
-				{'m':'generate_week_summary'},
-					function(data){
-						download_file(data);
-					}
-				,{'order':'t2.level,p.lastname,p.firstname','sport_id':$(this).parents('[iclass]').attr('iid'), 'week_id':$('[iclass="week"][description="'+$(this).parents('[container]').first().attr('container')+'"]').attr('iid')});
-			break;
-	}
-	if(iids.length>0){
-		apiCall(
-			{'m':'generate_stage_summary'},
-				function(data){
-					for(var i=0;i<data.length;i++){
-						download_file(data[i]);
-					}
-				}
-			,{'iid':iids});
-	}
-	return false;
-});
 
 function select_menu_item(route_url){
 	$('#menu>div.selected').removeClass('selected');
@@ -434,33 +133,4 @@ function warpcore_float_input() {
             }
         });
     }
-    
-}
-
-function highlight_next_element(parent_element,item_selector,highlight_class,direction='next'){
-	var step=1;
-	var current=parent_element.find(item_selector+'.'+highlight_class);
-	var all_list_items=parent_element.find(item_selector+':visible');
-	if(direction=='prev'){
-		step=-1;
-	}
-	$(current).removeClass(highlight_class);
-	var next=all_list_items.eq( all_list_items.index( $(current) ) + step );
-	if(next.length==0){
-		parent_element.find(item_selector+':visible').first().addClass(highlight_class);
-	}else{
-		next.addClass(highlight_class);
-	}
-}
-
-function scroll_to_element(container,target){
-	target=$(target);
-	container=$(container);
-	if (target.position()['top'] + target.outerHeight() + container.scrollTop() ==  container[0].scrollHeight ){
-		container.scrollTop(container[0].scrollHeight);		
-   }else if(target.position()['top'] < 0){
-		container.scrollTop(container.scrollTop() + target.position()['top']);
-   }else if(target.position()['top'] > container.height() - target.outerHeight()){
-		 container.scrollTop((container.scrollTop() + target.position()['top']) - (container.height()- target.outerHeight()));
-   }
 }
--- allow/state.js
+++ allow/state.js
@@ -1,8 +1,4 @@
 ﻿var global_object = {};
-query_iclass_register['get_stages']=['stage','sport','week','season'];
-query_iclass_register['get_abo']=['stage','sport','season'];
-query_iclass_register['get_teacher_availability']=['teacher','season'];
-query_iclass_register['get_people_stages']=['stage','people'];
 
 window.onbeforeunload = function () {
 	if($('body').attr('has_unsaved_data')=='true'||$('.container_right_state_wrapper[state="documents"] .table_content').attr('has_unsaved_data')=='true'){
@@ -50,131 +46,22 @@ function route(new_route, is_back) {
 }
 
 var states={
-	//WHERE TO_TIMESTAMP(w.date)>NOW()-INTERVAL '7 days'
-	'stages':{
+	'settings':{
+		'label':'Settings',
 		'function':function(){
-			return painters.button({'iclass_1':'season','iclass_2':'season_type','iid_2':1},'create_related','Ajouter une période')+create_ui_element({
-				'ui_element':{
-					'element_class':'table_list',
-					'selected_iids':[],
-					'hidden':['iclass','payment_status'],
-					'inline_attributes':['payment_status'],
-					'CSSclass':'stages',
-					'title_button':{
-						'1':[
-							{'class':'edit_season','label':''},
-							{'class':'add_week','label':''}
-						],
-						'2':[
-							{'class':'edit_week','label':''},
-							{'class':'add_stage','label':''}
-						],
-						'3':[
-							{'class':'edit_stage','label':''},
-							{'class':'print_stage','label':''}
-						],
-						'4':[
-							{'class':'edit_stage','label':''},
-							{'class':'add_participant','label':''}
-						]
-					},
-					'actions':[{'CSSclass':'remove_participant'}]
-				},
-				'api_call':{'pa':{'id':'','description': 'get_stages','db_id': 'this','m':'run_query'}}
-			});
+			return '';
 		}
 	},
-/*
-Rajouter en attachment au mail d'inscription un fichier de descriptif du stage qui aurait été lié au stage lors de sa création, il faut donc modifier la création / édition de stage
-*/
-	'participants':{'function':function(){
-			return painters.button({'iclass_1':'people'},'create_related','Ajouter un participant')+create_ui_element({
-				'ui_element':{
-					'CSSclass':'people_list',
-					'element_class':'table_list',
-					'selected_iids':[],
-					'attributes':['firstname','lastname','email','phone','city','phone2','old_id','proficiency','insurance','lat','lng','address','zipcode','birth_date'],
-					'hide_header':false,
-					'iclass':'people',
-					'hidden':['lat','lng','insurance','address','zipcode','birth_date'],
-					'inline_attributes':['insurance']
-				},
-				'api_call':{'pa':{'iclass':'people','search': '','m':'search_item'},'postcontent':{}}
-			});
+	'grants':{
+		'label':'Grants',
+		'function':function(){
+			return '';
 		}
 	},
-/*
-// trop pas fini 
-	'abonnements':{
+	'effort_reporting':{
+		'label':'Effort Reporting',
 		'function':function(){
-			return painters.button({'iclass_1':'season','iclass_2':'season_type','iid_2':2},'create_related','Ajouter une période')+create_ui_element({
-				'ui_element':{
-					'element_class':'table_list',
-					'selected_iids':[],
-					'title_button':{
-						'1':[
-							{'class':'edit_season','label':'Editer cette période'},
-							{'class':'add_abo','label':'Ajouter un abonnement'}
-						]
-					}
-				},
-				'api_call':{'pa':{'id':'','description': 'get_abo','db_id': 'this','m':'run_query'}}
-			});
-		}
-	},
-// trop pas fini 
-	'locations':{
-		'function':function(){
-			return painters.button({'iclass_1':'season','iclass_2':'season_type','iid_2':3},'create_related','Ajouter une période')+create_ui_element({
-				'ui_element':{
-					'element_class':'table_list',
-					'selected_iids':[],
-					'title_button':{
-						'1':[
-							{'class':'edit_season','label':'Editer cette période'},
-							{'class':'add_rental','label':'Ajouter une location'}
-						]
-					}
-				},
-				'api_call':{'pa':{'id':'','description': 'get_rentals','db_id': 'this','m':'run_query'}}
-			});
-		}
-	},
-	'moniteurs':{
-		'function':function(){
-			return painters.button({'iclass_1':'teacher'},'create_related','Ajouter un moniteur')+create_ui_element({
-				'ui_element':{
-					'CSSclass':'teacher_list',
-					'element_class':'table_list',
-					'selected_iids':[],
-					'iclass':'teacher'
-				},
-				'api_call':{'pa':{'iclass':'teacher','search': '','m':'search_item'},'postcontent':{}}
-			});
-		}
-	},
-// pas utilisable en pratique, peut-être pas vraiment utile non plus 
-	'paiements':{
-		'function':function(){
-			return create_ui_element({
-				'ui_element':{
-					'CSSclass':'payment_list',
-					'element_class':'table_list',
-					'selected_iids':[],
-					'iclass':'stage',
-					'hidden':['css_class','id_1'],
-					'inline_attributes':['css_class']
-				},
-				'api_call':{'pa':{'id':'','description': 'get_payments','db_id': 'this','m':'run_query'}}
-			});
+			return '';
 		}
 	}
-*/
-/*
-Recherche de participants:
-
-Interface de recherche avec 3+ filtres sur stage et date de naissance notamment
-Bouton export CSV
-Bouton pour récupérer juste la liste des adresses e-mails uniques de la liste de résultats
-*/
 }
